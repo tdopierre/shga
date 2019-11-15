0e928d8beb0e43791c7d0d5a950d6136fc43eefa
tdopierre <thomas.dopierre@gmail.com>
2019-11-15 10:33:12 +0100

    - Adds leboncoin support - Clean-up app.py code - Adds .venv/ to .gitignore

--- app.py
+++ app.py
@@ -1,20 +1,48 @@
 import telegram
-from util import AppConfig, CacheHandler, get_vinted_items_from_url
+from util import AppConfig, CacheHandler, get_vinted_items_from_url, get_lbc_items_from_url
 
+# Load globals
 config = AppConfig.load_from_disk('config.json')
 bot = telegram.Bot(token=config.telegram_token)
 cache_handler = CacheHandler()
 
-for url in config.vinted_urls:
 
-    vinted_items = get_vinted_items_from_url(url)
+def process_vinted_urls():
+    for url in config.vinted_urls:
 
-    for vinted_item in vinted_items:
-        # print(vinted_item.to_html())
-        if vinted_item.product_url in cache_handler:
-            continue
-        cache_handler.add(vinted_item.product_url)
-        bot.send_message(chat_id=config.telegram_chat_id,
-                         text=vinted_item.to_html(),
-                         parse_mode=telegram.ParseMode.HTML)
-        cache_handler.save()
+        vinted_items = get_vinted_items_from_url(url)
+
+        for vinted_item in vinted_items:
+            # print(vinted_item.to_html())
+            if vinted_item.url in cache_handler:
+                continue
+            cache_handler.add(vinted_item.url)
+            bot.send_message(chat_id=config.telegram_chat_id,
+                             text=vinted_item.to_html(),
+                             parse_mode=telegram.ParseMode.HTML)
+            cache_handler.save()
+
+
+def process_lbc_urls():
+    for url in config.lbc_urls:
+
+        lbc_items = get_lbc_items_from_url(url)
+
+        for item in lbc_items:
+            # print(vinted_item.to_html())
+            if item.url in cache_handler:
+                continue
+            cache_handler.add(item.url)
+            bot.send_message(chat_id=config.telegram_chat_id,
+                             text=item.to_html(),
+                             parse_mode=telegram.ParseMode.HTML)
+            cache_handler.save()
+
+
+def main():
+    process_vinted_urls()
+    process_lbc_urls()
+
+
+if __name__ == "__main__":
+    main()
--- util.py
+++ util.py
@@ -7,7 +7,7 @@ import json
 
 def clean_txt(txt):
     txt = re.sub(r'\u200b', r'', txt)
-    txt = re.sub(r'[ ]+', ' ', txt)
+    txt = re.sub(r'[\s]+', ' ', txt)
     return txt.strip()
 
 
@@ -21,29 +21,84 @@ def get_vinted_items_from_url(url):
         yield VintedItem.load_from_item_box__container(item)
 
 
+def get_lbc_items_from_url(url):
+    session = requests.session()
+    headers = {
+        # 'User-Agent': '*',
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36 OPR/64.0.3417.47",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+        "Accept-Language": "fr-FR,en;q=0.5",
+        "Accept-Encoding": "gzip, deflate",
+        "Connection": "keep-alive",
+        "Cache-Control": "max-age=0",
+        "Upgrade-Insecure-Requests": "1",
+    }
+    session.headers.update(headers)
+    url = "https://www.leboncoin.fr/recherche/?text=surly"
+
+    r = session.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    items = soup.find_all("li", {"class": "_3DFQ-"})
+    for item in items:
+        yield LBCItem.load_from_page(item)
+
+
+class LBCItem:
+    def __init__(self, price, name, url):
+        self.price = price
+        self.name = name
+        self.url = url
+
+    @classmethod
+    def load_from_page(cls, item):
+        name = clean_txt(item.find_all("span", {"itemprop": "name"})[0].text)
+        price = clean_txt(item.find_all("span", {"itemprop": "priceCurrency"})[0].text)
+        url = 'https://www.leboncoin.fr' + item.find_all("a", {"class": "clearfix trackable"})[0].attrs['href']
+
+        return cls(
+            price=price, name=name, url=url
+        )
+
+    def to_html(self):
+        html = ''
+        for key, val in self.__dict__.items():
+            if val is not None:
+                if val.startswith('http'):
+                    html += f'<a href="{val}">link</a>' + '\n'
+                else:
+                    html += val + '\n'
+        return html
+
+
 class AppConfig:
-    def __init__(self, telegram_token, telegram_chat_id, vinted_urls=list()):
+    def __init__(self, telegram_token, telegram_chat_id, vinted_urls=None, lbc_urls=None):
+        if vinted_urls is None:
+            vinted_urls = list()
+        if lbc_urls is None:
+            lbc_urls = list()
         self.telegram_token = telegram_token
         self.telegram_chat_id = telegram_chat_id
         self.vinted_urls = vinted_urls
+        self.lbc_urls = lbc_urls
 
     @classmethod
     def load_from_disk(cls, path):
         with open(path, 'r') as file:
             config = json.load(file)
         return cls(
-            telegram_token=config["telegram_token"],
-            telegram_chat_id=config["telegram_chat_id"],
-            vinted_urls=config["vinted_urls"]
+            telegram_token=config.get("telegram_token"),
+            telegram_chat_id=config.get("telegram_chat_id"),
+            vinted_urls=config.get("vinted_urls"),
+            lbc_urls=config.get("lbc_urls")
         )
 
 
 class VintedItem:
-    def __init__(self, price, size, product_url, desc=None, brand=None):
+    def __init__(self, price, size, url, desc=None, brand=None):
         self.brand = brand
         self.price = price
         self.size = size
-        self.product_url = product_url
+        self.url = url
         self.desc = desc
 
     @classmethod
@@ -55,9 +110,9 @@ class VintedItem:
         item_box_details = item.findAll("div", {"class": "item-box__details"})[0]
         price = item_box_details.findAll("div", {"class": "item-box__title"})[0].text.strip()
         size = item_box_details.findAll("div", {"class": "item-box__subtitle"})[0].text.strip()
-        product_url = f'https://www.vinted.fr{item.findAll("a", {"class": "media__image-wrapper js-item-link"})[0].attrs["href"]}'
+        url = f'https://www.vinted.fr{item.findAll("a", {"class": "media__image-wrapper js-item-link"})[0].attrs["href"]}'
         # desc = clean_txt(item.findAll("div", {"class": "media-caption__body"})[0].text)
-        return cls(brand=brand, price=price, size=size, product_url=product_url)
+        return cls(brand=brand, price=price, size=size, url=url)
 
     def to_html(self):
         html = ''
